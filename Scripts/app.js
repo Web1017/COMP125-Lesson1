082d401140d60ad525b83eddbf27399b564e24b2
Web1017 <j-n_01@hotmail.com>
2016-05-11 18:59:03 -0400

    first commit

---
+++ Scripts/app.js
@@ -0,0 +1,12 @@
+// Custom javascript
+// self-executing anonymous function
+// IIFE - Immediately invoked Function Expression
+(function() {
+console.log("App Started...");
+
+//Variable declaration and Initialization
+var mnumber = 0;
+var number = Math.floor((Math.random() * 10) + 1);
+
+console.log(number);
+})();
